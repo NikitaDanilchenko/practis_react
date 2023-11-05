e8d39815009888b1c28ea7e3944e5a3e40f229c9
Shamanyaaa <v.pone@email.ru>
2023-11-05 17:01:10 +0300

    postIdPage

--- src/App.js
+++ src/App.js
@@ -1,25 +1,15 @@
 import React from "react";
 import "./styles/App.css";
-import { BrowserRouter, Link, Route, Routes } from "react-router-dom";
-import About from "./pages/About";
-import Posts from "./pages/Posts";
+import { BrowserRouter } from "react-router-dom";
+import Navbar from "./components/UI/Navbar/Navbar";
+import AppRouter from "./components/AppRouter";
 
 
 function App() {
   return (
     <BrowserRouter>
-    <div className="navbar">
-      <div className="navbar__links">
-        <Link to="/about">О сайте</Link>
-        <Link to="/posts">Посты</Link>
-        </div>
-    </div>
-      <Routes>
-        <Route path="/about" element={<About />} />
-      </Routes>
-      <Routes>
-        <Route path="/posts" element={<Posts />} />
-      </Routes>
+    <Navbar/>
+      <AppRouter/>
     </BrowserRouter>
   )
 }
